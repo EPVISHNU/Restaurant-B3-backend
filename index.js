49548f5dda44707c53d858fd9ca3715bc73d3d76
EPVISHNU <vishnuep8089@gmail.com>
2024-01-09 23:21:01 +0530

    Backend for restaurant-app

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer=require('json-server')
+
+const restServer= jsonServer.create()
+
+const router=jsonServer.router()
+
+const middleware=jsonServer.defaults()
+
+const port = 3001
+
+restServer.use(middleware)
+restServer.use(router)
+
+restServer.listen(port,()=>{
+console.log('Rest server listening on port'+port);
+})
